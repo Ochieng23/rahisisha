53775931a3da0dbe4137776950311c19c9832b75
+ <+>
2023-04-19 18:01:55 +0300

    Feature Create User 1. Handle SignUp form 2. Create functionality that creates user and assigns a jwt token

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,45 +1,43 @@
-import React from 'react'
+import React from 'react';
 import Footer from './Footer';
-
 
 function Login() {
   return (
     <>
-    <div style={{ color: "white", height: "100vh"}}>
-  <div className="overlay">
-    <div className="login-form-container border rounded p-3 " style={{width:"45vw",height:"60vh",backgroundColor:"black", marginLeft:"300px"}}>
-      <div className="login-form-overlay">
-        <div className="login-form-container">
-          <div className="login-form-header">
-            <h2>Log in</h2>
-          </div>
-  
-          <div className="login-form-body">
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">Email:</label>
-              <input type="email" className="form-control" id="email" />
-            </div>
-  
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">Password:</label>
-              <input type="password" className="form-control" id="password" />
-            </div>
-  
-            <button className="btn btn-primary">Log in</button>
-  
-            <div className="signup-link-container mt-3">
-              <p>Don't have an account?</p>
-              <button className="btn btn-outline-primary">Sign up</button>
+      <div style={{ color: "white", minHeight: "100vh" }} className="d-flex align-items-center">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <div className="login-form-container border rounded p-3" style={{ backgroundColor: "black" }}>
+                <div className="login-form-header">
+                  <h2>Log in</h2>
+                </div>
+
+                <div className="login-form-body">
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email:</label>
+                    <input type="email" className="form-control" id="email" />
+                  </div>
+
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password:</label>
+                    <input type="password" className="form-control" id="password" />
+                  </div>
+
+                  <button className="btn btn-primary">Log in</button>
+
+                  <div className="signup-link-container mt-3">
+                    <p>Don't have an account?</p>
+                    <button className="btn btn-outline-primary">Sign up</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-<Footer/>
-</>
-
+      <Footer />
+    </>
   )
 }
 
